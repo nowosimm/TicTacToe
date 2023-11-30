5938a1b9be488e27b69d773860464d31ad29259f
Maddie <nowosimm@mail.uc.edu>
2023-11-30 14:40:30 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let valueX = "x"
+let valueO = "o"
+
+
+
+function playGame() {
+    // initail value starts as x
+    // on click, x fills the div and the value changes to O
+    // on click, O fills the div and value changes back to O
+}
+
+function playerSwitch() {
+    // when valueX = true, message should say "player x's turn"
+    // when valueO = true, message should say "player o's turn" 
+}
+
+function announceWinner() {
+    // using the h2 id tag, announce who won based on:
+    // 1) x is 3 in a row, player x wins
+    // 2) o is 3 in a row, player o wins
+    // 3) the 9 divs are all full without 3 in a row, resulting in a tie
+}
+
+function restartGame() {
+
+}
+
+
+
+
+function sayHello(name) {
+    console.log("hello "+ name)
+}
+
+
+
+
+
+
+
+
